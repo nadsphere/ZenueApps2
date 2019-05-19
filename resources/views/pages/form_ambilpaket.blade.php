cdff25a1ada6dc08ad95fc6352df221a5b774fe8
Nadia Hikmah <nadeetsu19@github.com>
2019-05-19 15:06:43 +0700

    ambil paket blade
    form ambil paket (mohon maaf klo ada salah tulis)

--- resources/views/pages/form_ambilpaket.blade.php
+++ resources/views/pages/form_ambilpaket.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>My Request</title>
+  <title>Pengambilan Paket</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link href="img/favicon.png" rel="icon">
     <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
@@ -14,15 +14,7 @@
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/fix.css" rel="stylesheet" type="text/css"/>
     <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <style>
-        .img-circle{
-          width: 100px;
-          height: 100px;
-        }
-        .txt-small{
-          font-size: 15px;
-      }
-    </style>
+
 </head>
 <body>
     <button type="button" class="mobile-nav-toggle d-lg-none"><i class="fa fa-bars"></i></button>
@@ -131,28 +123,85 @@
         <section class="container">
             <br />  <br />  <br /> <br />
             <div class=" bg-white" style="margin-top:50px">
-
+                    <br /><br />  
+                    <header class="section-header">
+                        <h3 class="title">Form Ambil Paket</h3>
+                    </header>
                     <br /><br />
                     <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-10">
-                            <table class="table">
-                                <tr>
-                                    <th> Nama Paket</th>
-                                    <td>Paket Nikah (Kelas Ekonomi)</td>
-                                    <tr></tr>            
-                                    <th>Fasilitas</th>
-                                    <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td> 
-                                    <tr></tr>
-                                    <th>Harga</th>
-                                    <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td>
-                                    <tr></tr>
-                                </tr>
-                            </table>
-
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8">
+                            <form action="">
+                                <div class="form-group row">
+                                    <label for="nama_cust" class="col-sm-3 col-form-label"><b> Nama Pembeli</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control-plaintext" id="nama_cust" value="Joko Mulyadi" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="email" class="col-sm-3 col-form-label"><b> Email</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="email" class="form-control" id="email" placeholder="" name="email" value="jokomul13@gmail.com">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="telpon" class="col-sm-3 col-form-label"><b> Nomor Telepon</b></label>
+                                    <div class="col-sm-9">
+                                      <input type="number" class="form-control" name="no_telpon" id="telpon" placeholder="0813xxxx" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="kode_book" class="col-sm-3 col-form-label"><b> Kode Booking</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control-plaintext" value="EO-1234567" id="kode_book" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="nama_barang" class="col-sm-3 col-form-label"><b> Nama Paket yang Diambil</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="nama_barang" name="nama_barang" placeholder="" name="email" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="des_pkt" class="col-sm-3 col-form-label"><b>Deskripsi</b></label>
+                                    <div class="col-sm-9">
+                                        <textarea name="deskrisi_pkt" class="form-control" name="" id="des_pkt" rows="3"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="tgl_ambil" class="col-sm-3 col-form-label"><b>Tanggal Ambil</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="tgl_ambil" name="nama_barang" placeholder="" name="email" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="harga_paket" class="col-sm-3 col-form-label"><b>Harga Paket</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="tgl_ambil" name="nama_barang" placeholder="" name="harga_paket" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="harga_paket" class="col-sm-3 col-form-label"><b>Pembayaran</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control-plaintext" id="tgl_ambil" name="nama_barang" placeholder="" name="harga_paket" value="Transfer" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="harga_paket" class="col-sm-3 col-form-label"><b>Status Pembayaran</b></label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control-plaintext" id="tgl_ambil" name="nama_barang" placeholder="" name="harga_paket" value="Sudah Membayar Uang Penuh" readonly>
+                                    </div>
+                                </div>
+                                <br />
+                                <div class="text-center">
+                                    <button class="btn btn-outline-danger" type="reset">Kembali</button>
+                                    <button class="btn btn-danger" type="submit">Ambil Paket</button>
+                                </div>
+                                <br /><br />
+                            </form>
                         </div>
-                        <div class="col-md-1"></div>
-                    </div>
+                        <div class="col-md-2"></div>
+                      </div>
                     <br />
                     
 
