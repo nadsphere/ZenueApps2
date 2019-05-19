6b80291c928a25abc230b49eaa4f1a69bc97e1b2
Nadia Hikmah <nadeetsu19@github.com>
2019-05-19 09:42:30 +0700

    form blade
    Ada beberapa yang gw rapihin

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -6,78 +6,189 @@
   <title>AdminLTE 2 | General Form Elements</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <!-- Favicons -->
+    <link href="img/favicon.png" rel="icon">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
+    <link href="css/ui.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/fix.css" rel="stylesheet" type="text/css"/>
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 </head>
-<div>
-<table class="table">
-        <tr>
-            <th>Nama Paket</th>
-            <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td>
-            <tr></tr>            
-            <th>Fasilitas</th>
-            <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td> 
-            <tr></tr>
-            <th>Harga</th>
-            <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td>
-            <tr></tr>
-        <tr>
-        </tr>
-    </table>
-    </div>
+<body>
+    <button type="button" class="mobile-nav-toggle d-lg-none"><i class="fa fa-bars"></i></button>
+    <header id="header" class="header-stack">
+      <div class="container">
+        <div class="logo float-left">
+          <h1 class="text-light"><a href="promaag.html" class="scrollto"><span>ZEN</span></a></h1>
+        </div>
+    
+        <nav class="main-nav float-right d-none d-lg-block">
+          <ul>
+            <li>
+                <a href="#" class="widget-header mr-3">
+                  <i style="font-size: 16pt" class="icon fa fa-shopping-basket"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="widget-header mr-3">
+                  <i style="font-size: 16pt" class="icon fa fa-bell-o"></i>
+                </a>
+            </li>
+            <li>
+                <div class="input-group">
+                    <div class="form-group has-search">
+                        <span class="fa fa-glip fa-search form-control-feedback"></span>
+                        <input type="text" class="form-controls form-control" placeholder="Cari...">
+                    </div>
+                </div>
+            </li>
+            <!-- <li><a href="after-login.html">Iklan</a></li> -->
+            <li class="drop-down"><a href="index.html"><span>Joko Mulyadi</span></a>
+              <ul>
+                <li><a href="#">Edit Profil</a></li>
+                <li><a href="#">My Order</a></li>
+                <li><a href="#">My Wishlist</a></li>
+                <li><a href="index.html">Sign Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main id="main">
+        <section class="bg-white"> 
+            <div class="container" style="margin-top:75px">
+                <br /><br />
+                <header class="section-header">
+                    <h3 class="title">Paket Anda</h3>
+                </header>
+                <br />
+                <table class="table">
+                    <tr>
+                        <th>Nama Paket</th>
+                        <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td>
+                        <tr></tr>            
+                        <th>Fasilitas</th>
+                        <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td> 
+                        <tr></tr>
+                        <th>Harga</th>
+                        <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloribus sequi nihil fugiat laudantium ipsam, ducimus pariatur reiciendis officiis et nostrum blanditiis dolorum dolorem odio soluta quae optio veniam? Veritatis?</td>
+                        <tr></tr>
+                    </tr>
+                </table>
+                <div>
+                  <form action="">
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">Nama Pembeli</label>
+                      <input type="text" class="form-control" id="exInputNama1">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Alamat</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">Nomor Telpon</label>
+                      <input type="number" class="form-control" id="exInputTelpon1" placeholder="Nomor Telpon">
+                    </div> 
+                    <div class="form-group">
+                          <label>Pilih Pembayaran</label>
+                          <select class="form-control">
+                            <option>Transfer</option>
+                            <option>Midtrans</option>
+                          </select>
+                    </div>
+                    <button class="btn btn-danger" type="submit">Submit</button>
+                    <button class="btn btn-danger" type="submit">Reset</button>
+                  </form>
+                </div>
+            </div>                    		                            
+        </section> 
+        
+    </main>
+    <br /><br /><br /><br />
+      <footer id="footer" class="section-bg">
+        <div class="footer-top">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-3">
+                      <div class="footer-links">
+                          <h4>Mitra EO Zen</h4>
+                            <ul>
+                              <li><a href="#">Cara Jual</a></li>
+                              <li><a href="#">Daftar Mitra</a></li>
+                              <li><a href="#">Ketentuan Dana</a></li>
+                              <li><a href="#">Periklanan</a></li>
+                            </ul>
+                        </div>
+                  </div>  
+                  <div class="col-sm-3">
+                      <div class="footer-links">
+                          <h4>Lebih Tahu Zenith</h4>
+                            <ul>
+                              <li><a href="#">Tentang Kami</a></li>
+                              <li><a href="#">Layanan</a></li>
+                              <li><a href="#">Syarat dan Ketentuan</a></li>
+                              <li><a href="#">Kebijakan Privasi</a></li>
+                            </ul>
+                        </div>
+                  </div>
+                  <div class="col-sm-3">
+                    <div class="footer-links">
+                      <h4>Layanan Pelanggan</h4>
+                        <ul>
+                          <li><a href="#">Pembayaran</a></li>
+                          <li><a href="#">Pengembalian Dana dan Uang</a></li>
+                          <li><a href="#">Komplain</a></li>
+                          <li><a href="#">Garansi</a></li>
+                          <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </div>
+                  </div>
+                  <div class="col-sm-3">
+                    <div class="footer-links">
+                      <h4>Contact Us</h4>
+                      <p>
+                        Jl. Kemanggisan VII Kav. 31 <br>
+                        Jakarta Selatan<br>
+                        <strong>Phone:</strong> +62 (21)123456<br>
+                        <strong>Email:</strong> info@example.com<br>
+                      </p>
+                    </div>
+                  </div>
+              </div>
+              <div class="copyright">
+                  <div class="social-links">
+                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                  </div>
+                  <br />
+                  <p class="text-center">&copy; 2019 Copyright <strong>Zenith </strong>| Hak Cipta Dilindungi</p>
+              </div> 
+          </div>
+        </div>
+      </footer>
 
-    <div class="form-group">
-                  <label for="exampleFormControlInput1">Nama Pembeli</label>
-                  <input type="text" class="form-control" id="exInputNama1">
-    </div>
 
-    <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exInputEmail1" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-                  <label for="exampleFormControlInput1">Nomor Telpon</label>
-                  <input type="number" class="form-control" id="exInputTelpon1" placeholder="Nomor Telpon">
-    </div> 
+      
+  <!-- ./wrapper -->
 
-    <div class="form-group">
-                  <label>Pilih Pembayaran</label>
-                  <select class="form-control">
-                    <option>Transfer</option>
-                  </select>
-    </div>
-    <button class="btn btn-danger" type="submit">Submit</button>
-    <button class="btn btn-danger" type="submit">Reset</button>
-<!-- ./wrapper -->
-
-<!-- jQuery 3 -->
-<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- FastClick -->
-<script src="../../bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+  <script src="../../dist/js/demo.js"></script>
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="js/main.js"></script>
 </body>
 </html>
